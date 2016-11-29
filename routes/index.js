bea9e3ee38b5056bb8e964253884cc12d2c190ad
Swaroop Raja Srinivas Setty <swaroop.rajasrinivasset@mavs.uta.edu>
2016-11-29 00:22:05 -0600

    final changes

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,27 @@ var assert = require('assert');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
 
 /*router.get('/get-data', function(req, res, next) {
-  var resultArray = [];
-  var id = req.param('param1');
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    var cursor = db.collection('sacramentoRealEstateTransactions').find({ "id": parseInt(id)},{ _id:0,sale_date:0,latitude:0,longitude:0});
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      resultArray.push(doc);
-    }, function() {
-      db.close();
-        console.log(resultArray);
-      res.json(resultArray);
-    });
-  });
-}); */
+ var resultArray = [];
+ var id = req.param('param1');
+ mongo.connect(url, function(err, db) {
+ assert.equal(null, err);
+ var cursor = db.collection('sacramentoRealEstateTransactions').find({ "id": parseInt(id)},{ _id:0,sale_date:0,latitude:0,longitude:0});
+ cursor.forEach(function(doc, err) {
+ assert.equal(null, err);
+ resultArray.push(doc);
+ }, function() {
+ db.close();
+ console.log(resultArray);
+ res.json(resultArray);
+ });
+ });
+ }); */
 
 /* GET data from Mongodb */
 router.get('/get-data', function(req, res) {
